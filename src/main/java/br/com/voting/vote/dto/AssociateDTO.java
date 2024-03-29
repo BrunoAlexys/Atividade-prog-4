0e501dfd409012364b9826f5830f119eaf829cff
Bruno Moura <124813743+BrunoAlexys@users.noreply.github.com>
2024-03-29 18:40:21 -0300

    Atualização

--- src/main/java/br/com/voting/vote/dto/AssociateDTO.java
+++ src/main/java/br/com/voting/vote/dto/AssociateDTO.java
@@ -1,7 +1,13 @@
 package br.com.voting.vote.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
+
 public record AssociateDTO(
+        @NotBlank(message = "Name is required")
         String name,
+        @NotBlank(message = "CPF is required")
+        @CPF(message = "CPF is invalid")
         String cpf
 
 ) {
